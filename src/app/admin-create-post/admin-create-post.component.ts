8bd7be929937dc55e3b5e11163352ff50210ea88
fneel <70952301+fneel@users.noreply.github.com>
2023-11-11 23:45:12 +0100

    ihopslagen med server

--- src/app/admin-create-post/admin-create-post.component.ts
+++ src/app/admin-create-post/admin-create-post.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { BlogPost } from '../blog-post.model';
 import { BlogPostService } from '../blog-post.service';
+// Importera HttpClient i din komponent (t.ex. admin-create-post.component.ts)
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-admin-create-post',
@@ -17,8 +19,17 @@ export class AdminCreatePostComponent {
     dislikes: 0,
     comments: [],
   };
+  // Lägg till en array för att hålla listan över uppladdade bilder
+  uploadedImages: string[] = [];
 
-  constructor(private blogPostService: BlogPostService) {}
+  // Injicera HttpClient i konstruktorn
+  constructor(
+    private blogPostService: BlogPostService,
+    private http: HttpClient
+  ) {
+    // Hämta tidigare uppladdade bilder vid komponentinitialisering
+    this.getUploadedImages();
+  }
 
   createBlogPost() {
     this.blogPostService.createBlogPost(this.newBlogPost);
@@ -31,5 +42,18 @@ export class AdminCreatePostComponent {
       dislikes: 0,
       comments: [],
     };
+  }
+
+  // Lägg till en funktion för att hämta tidigare uppladdade bilder från servern
+  getUploadedImages() {
+    this.http
+      .get<any>('http://localhost:4000/uploads')
+      .subscribe((response) => {
+        if (response.success) {
+          this.uploadedImages = response.files;
+        } else {
+          console.error('Error fetching uploaded images:', response.error);
+        }
+      });
   }
 }
